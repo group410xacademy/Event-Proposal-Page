5079fca684bf864387fe324dffe523c15e0105e0
meel516 <md.saleem516@gmail.com>
2023-08-10 15:32:57 +0530

    sample route

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
 require('dotenv').config()
 const express =require('express')
 const app=express()
+app.listen(process.env.PORT,()=>{console.log('listening on port'+process.env.PORT)})
+app.get('/',(req,res)=>{res.send('welcome')})
