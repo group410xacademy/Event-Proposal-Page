fa3d611be81afcac66e50805465ed8e1b50e602d
meel516 <md.saleem516@gmail.com>
2023-08-10 15:19:29 +0530

    added connector.js

---
+++ backend/connector.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+let mongoose = require('mongoose')
+const {userSchema} = require('./model/user')
+const {vendorSchema} = require('./model/vendor')
+mongoose.connect(process.env.CONNECTION_STRING,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>console.log('conection established with mongodb server')).catch(err=>{
+    console.log('error while connection',err)
+})
+userDB=mongoose.model('user',userSchema)
+vendorDB=mongoose.model('vendor',vendorSchema)
+
+
+exports.userDB=userDB
+exportS.vendorDB=vendorDB
